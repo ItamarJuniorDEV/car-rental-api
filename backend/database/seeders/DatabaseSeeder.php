590d3c40de90e9d7932c8486be4cb082fb20dbcf
Itamar Junior <cdajuniorf@gmail.com>
2025-09-07 14:30:00 -0300

    feat: seeders com dados reais de carros, clientes e locações

---
+++ backend/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@locadora.com',
+            'password' => bcrypt('senha123'),
+            'role' => 'admin',
+        ]);
+
+        $this->call([
+            CarSeeder::class,
+            ClientSeeder::class,
+            RentalSeeder::class,
+        ]);
+    }
+}
